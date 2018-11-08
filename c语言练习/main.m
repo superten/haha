1191fafe813b6c40c2bc04b22f8ae41728f8f379
ryw <464139951@qq.com>
2018-11-08 11:51:12 +0800

    xx

---
+++ c语言练习/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  c语言练习
+//
+//  Created by yw on 2018/11/7.
+//  Copyright © 2018 yw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "stdio.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int n = 2;
+        int *i = &n;
+                   //应该为 *(i ++);
+            printf("%d ", *
+        
+
+    }
+    return 0;
+}
